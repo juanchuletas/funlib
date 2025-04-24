e8021c787a4161c6153376e35b8385c82512eb07
juanchuletas <juan.garcia.cpp@gmail.com>
2025-04-23 19:37:41 -0600

    first commit

---
+++ include/funlib.hpp
@@ -0,0 +1,10 @@
+#if !defined(_FUNLIB_H_)
+#define _FUNLIB_H_
+
+#include "../sycl/sycl_handler.hpp"
+#include "../ParticleSys/ParticleSystem.hpp"
+#include "../ParticleSet/particle_set.hpp"
+#include "../Set/set.hpp"
+#include "../Set/set_operations.hpp"
+#include "../LinearAlgebra/linal.hpp"
+#endif // _FLIB_H_
